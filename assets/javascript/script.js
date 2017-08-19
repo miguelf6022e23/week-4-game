1a67ecca952e693ca760cf43fe026259bd2c23f8
Miguel <miguelf6.022e23@gmail.com>
2017-08-19 11:35:33 -0400

    Done :)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,6 +7,11 @@ var state = {
 	// gem3val
 	// gem4val
 	// score
+	targEl:document.getElementById("target"),
+	scoreEl:document.getElementById("score"),
+	winsEl:document.getElementById("wins"),
+	lossEl:document.getElementById("losses"),
+	icons:document.getElementsByClassName("icon"),
 
 	reset: function(){
 		this.target= 20+Math.floor(Math.random()*20);
@@ -15,16 +20,37 @@ var state = {
 		this.gem3val= 2+Math.floor(Math.random()*7);
 		this.gem4val= 2+Math.floor(Math.random()*7);
 		this.score=0;
-
+		this.disp;
 },
 
 	disp: function(){
-		$(#target).HTML(this.target)
+		// console.log(this)
+		this.targEl.innerHTML = this.target;
+		this.winsEl.innerHTML = "Wins: " + this.wins;
+		this.lossEl.innerHTML = "Losses: " + this.losses;
+		this.scoreEl.innerHTML= this.score;
 	},
-
-
 
 }
 
-state.reset()
-console.log(state)
+state.reset();
+state.disp();
+// console.log(state);
+// console.log(state.icons);
+
+ $(document).ready(function() {
+        $('img').click(function() {
+            // console.log(this.id);
+            state.score += state[this.id+"val"];
+            if (state.score>state.target){
+            	alert("You Lost :(");
+            	state.losses++;
+            	state.reset();
+            } else if (state.score === state.target){
+            	alert("You Won!");
+            	state.wins++;
+            	state.reset();
+            }
+            state.disp();
+        });
+    });
